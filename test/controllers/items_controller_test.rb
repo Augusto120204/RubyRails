a3d7f6d67ec65d617857d2a0a0307c39acdfc196
Erick Lasluisa <erickl1709@gmail.com>
2024-06-08 19:11:03 -0500

    Test Unitarios para operaciones CRUD

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,23 +1,53 @@
-require "test_helper"
+require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get items_index_url
-    assert_response :success
+  setup do
+    @item = items(:one)
   end
 
-  test "should get show" do
-    get items_show_url
+  test "should get index" do
+    get items_url
     assert_response :success
+    assert_not_nil assigns(:items)
   end
 
   test "should get new" do
-    get items_new_url
+    get new_item_url
+    assert_response :success
+  end
+
+  test "should create item" do
+    assert_difference('Item.count') do
+      post items_url, params: { item: { name: 'New item', description: 'Description for new item', quantity: 1 } }
+    end
+
+    assert_redirected_to item_url(Item.last)
+  end
+
+  test "should show item" do
+    get item_url(@item)
     assert_response :success
   end
 
   test "should get edit" do
-    get items_edit_url
+    get edit_item_url(@item)
     assert_response :success
+  end
+
+  test "should update item" do
+    patch item_url(@item), params: { item: { name: 'Updated name', description: 'Updated description', quantity: 2 } }
+    assert_redirected_to item_url(@item)
+    @item.reload
+    assert_equal 'Updated name', @item.name
+    assert_equal 'Updated description', @item.description
+    assert_equal 2, @item.quantity
+  end
+
+  test "should destroy item" do
+    assert_difference('Item.count', -1) do
+      delete item_url(@item)
+    end
+
+    assert_redirected_to items_url
   end
 end
